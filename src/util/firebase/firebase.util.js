4265bd5c94c9e89a67e5aaec90784155c4768d37
Cannon Wong <cannonbwong@gmail.com>
2023-01-16 17:27:28 -0500

    first commit

--- src/util/firebase/firebase.util.js
+++ src/util/firebase/firebase.util.js
@@ -7,9 +7,19 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  getDocs,
+  query,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -42,6 +52,36 @@ export const signInWithGoogleRedirect = () => {
 //Access Firestore Database
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  
+  objectsToAdd.forEach((object) => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
   const userDocRef = doc(db, "users", userAuth.uid);
 
@@ -53,7 +93,12 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
     const createdAt = new Date();
 
     try {
-      await setDoc(userDocRef, { displayName, email, createdAt, ...additionalInfo });
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInfo,
+      });
     } catch (error) {
       console.log("Error creating the user", error.message);
     }
@@ -64,12 +109,12 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password ) return;
+  if (!email || !password) return;
 
-    return await createUserWithEmailAndPassword(auth, email, password);
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const signInAuthUserWithEmailandPassword = async (email, password) =>  {
+export const signInAuthUserWithEmailandPassword = async (email, password) => {
   if (!email || !password) return;
 
   return await signInWithEmailAndPassword(auth, email, password);
@@ -77,6 +122,5 @@ export const signInAuthUserWithEmailandPassword = async (email, password) =>  {
 
 export const signOutUser = async () => signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
-
-
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
