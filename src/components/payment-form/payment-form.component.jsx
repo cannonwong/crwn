10fc0b46ab32cea8ca4c8bfb501d99d0aa1e6492
Cannon <cannonbwong@gmail.com>
2023-01-31 17:12:05 -0500

    fixed google sign in button

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,13 +1,14 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useSelector } from 'react-redux';
 
-import { selectCartTotal } from "../../store/cart/cart.selector";
-import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectCartTotal } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
-import { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { FormContainer } from './payment-form.styles';
+import { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form.styles";
+import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -18,30 +19,27 @@ const PaymentForm = () => {
 
   const paymentHandler = async (e) => {
     e.preventDefault();
-
     if (!stripe || !elements) {
       return;
     }
-
     setIsProcessingPayment(true);
-
-    const response = await fetch("/.netlify/functions/create-payment-intent", {
-      method: "post",
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ amount: amount * 100 }),
-    }).then((res) => res.json());
+    }).then((res) => {
+      return res.json();
+    });
 
-    const {
-      paymentIntent: { client_secret },
-    } = response;
+    const clientSecret = response.paymentIntent.client_secret;
 
-    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: currentUser ? currentUser.displayName : "Guest",
+          name: currentUser ? currentUser.displayName : 'Yihua Zhang',
         },
       },
     });
@@ -49,10 +47,10 @@ const PaymentForm = () => {
     setIsProcessingPayment(false);
 
     if (paymentResult.error) {
-      alert(paymentResult.error);
+      alert(paymentResult.error.message);
     } else {
-      if (paymentResult.paymentIntent.status === "succeeded") {
-        alert("Payment Successful");
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful!');
       }
     }
   };
@@ -60,17 +58,16 @@ const PaymentForm = () => {
   return (
     <PaymentFormContainer>
       <FormContainer onSubmit={paymentHandler}>
-        <h2>Credit Card Payment: </h2>
+        <h2>Credit Card Payment:</h2>
         <CardElement />
         <PaymentButton
-          isLoading={isProcessingPayment}
           buttonType={BUTTON_TYPE_CLASSES.inverted}
+          isLoading={isProcessingPayment}
         >
-          Pay now
+          Pay Now
         </PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
 };
-
 export default PaymentForm;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -71,7 +71,7 @@ const SignInForm = () => {
             type='button'
             onClick={signInWithGoogle}
           >
-            Sign In With Google
+            Google Sign In
           </Button>
         </ButtonsContainer>
       </form>
